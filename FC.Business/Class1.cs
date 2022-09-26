c882ce0fc3bbe80a64d90f7dd44afb41409d2ebc
adamtaa <adamtaa@gmail.com>
2022-09-26 14:42:00 -0400

    Add project files.

---
+++ FC.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FC.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ FC.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FC.Data
+{
+    public class Class1
+    {
+    }
+}
